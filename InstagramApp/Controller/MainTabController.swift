f4cabec3b16b34afece4b0fb785106b0efbbfc8a
muhammed dursun <mamiankara10@gmail.com>
2024-05-09 13:30:30 +0300

    The file structure and Tabcontroller you will use in the project have been added.

---
+++ InstagramApp/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  InstagramApp
+//
+//  Created by muhammed dursun on 9.05.2024.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+    
+    
+}
